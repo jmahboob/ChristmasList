b610db3ae40f08bde2e8347fdc8d2f012e0e6207
Jamal Mahboob <jamal.mahboob@gmail.com>
2016-09-16 15:06:19 -0400

    added two basic routes (users and "wishes") and also imported a lot of the sqlalchemy functionality

--- christmaslist.py
+++ christmaslist.py
@@ -1,11 +1,88 @@
 #!/usr/bin/env python
 
-from flask import Flask
+import os
+import sys
+import datetime
+
+from sqlalchemy import Column, ForeignKey, Integer, String, Numeric, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+
+from flask import Flask, request, jsonify
 app = Flask(__name__)
+
+DB_NAME = "christmaslist"
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False)
+
+class Wish(Base):
+    __tablename__ = 'wish'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    description = Column(String(500))
+    link = Column(String(500))
+    cost = Column(Numeric)
+    created = Column(DateTime)
+
+def open_session():
+    engine = create_engine('sqlite:///' + DB_NAME + '.db')
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    return session
 
 @app.route("/")
 def root():
     return "Hello World!"
 
+@app.route("/create/user", methods=['POST'])
+def create_user():
+    data = request.get_json(force=True)
+    if data == None:
+        return "Error processing request.  Posted information must be a JSON object to create a new user"
+
+    engine = create_engine('sqlite:///' + DB_NAME + '.db')
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    new_user = User(
+                    first_name = data['first_name'],
+                    last_name = data['last_name'],
+                    email = data['email']
+    )
+    session.add(new_user)
+    session.commit()
+    session.close()
+    return jsonify(data)
+
+@app.route("/create/wish", methods=['POST'])
+def create_wish():
+    data = request.get_json(force=True)
+
+    session = open_session()
+
+    new_wish = Wish(
+        name = data['name'],
+        description = data['description'],
+        link = data['link'],
+        cost = data['cost'],
+        created = datetime.datetime.now()
+    )
+    session.add(new_wish)
+    session.commit()
+    session.close()
+    return jsonify(data)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    engine = create_engine('sqlite:///' + DB_NAME + '.db')
+    Base.metadata.create_all(engine)
+    app.run(host="0.0.0.0", debug=True)
